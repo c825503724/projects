b0ee6ae691a8889e6f4a87edfa1f23426e6fe2d2
cp <825503724@qq.com>
2018-08-06 14:49:38 +0800

    扩展hmi对外服务接口

--- HMI/src/main/java/com/anji/project/hmi/service/SerialPortService.java
+++ HMI/src/main/java/com/anji/project/hmi/service/SerialPortService.java
@@ -1,17 +1,31 @@
 package com.anji.project.hmi.service;
 
-import org.springframework.context.ApplicationListener;
+public interface SerialPortService  {
 
-public interface SerialPortService extends ApplicationListener {
+    /**
+     * 启动串口监听
+     */
+    void start();
 
-    //是否成功初始化
-    boolean init();
+    /**
+     * 终止串口监听
+     */
+    void stop();
 
+    /**
+     * 获取目前监听状态
+     * @return
+     */
     PortState getServiceState();
 
 
-    enum PortState {
+    Integer getMessageLengthNotConsumed();
 
+    /**
+     * 四种监听状态
+     */
+    enum PortState {
         NOT_START, FAIL, STOP,SUCCESS
     }
 }
+
